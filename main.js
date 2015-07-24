53bec600f7ada4e30377c4e717613b8d1140a23b
Krakabek <danilradkovsky@gmail.com>
2015-07-24 17:55:06 +0300

    WOW, that's awesome

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ angular.module('StackApp', []).config(function ($routeProvider) {
 	'use strict';
 
 	$routeProvider
-		.when('/', {
-			templateUrl: "./main_view.html",
-			controller: 'MainCtrl'
-		})
-		.otherwise({
-			redirectTo: '/'
-		});
+	.when('/', {
+		templateUrl: "main_view.html",
+		controller: 'MainCtrl'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
 
 });
 
@@ -18,25 +18,53 @@ angular.module('StackApp').controller('MainCtrl', function ($scope) {
 
 	$scope.reverse = 'false';
 
-	$scope.myList = [
-		{id:0, text:'HTML5 Boilerplate'},
-		{id:1, text:'AngularJS'},
-		{id:2, text:'Karma'},
-		{id:3, text:'Hello'},
-		{id:4, text:'World'},
-		{id:5, text:'How'},
-		{id:6, text:'Are'},
-		{id:7, text:'You'},
-		{id:8, text:'?'},
-		{id:9, text:'I'},
-		{id:10, text:'write'},
-		{id:11, text:'more'},
-		{id:12, text:'to'},
-		{id:13, text:'make'},
-		{id:14, text:'the'},
-		{id:15, text:'list'},
-		{id:16, text:'longer'}
+	var values=[
+		'2',
+		'3',
+		'4',
+		'5',
+		'6',
+		'7',
+		'8',
+		'9',
+		'10',
+		'jack',
+		'queen',
+		'king',
+		'ace'
 	];
+
+	var suits=[
+		'clubs',
+		'diamonds',
+		'hearts',
+		'spades'
+	];
+	$scope.myList=[];
+	values.forEach(function(value){
+		suits.forEach(function(suit){
+			$scope.myList.push({src: value + "_of_" + suit + ".png"});
+		});
+	});
+	// $scope.myList = [
+	// 	{id:0, text:'HTML5 Boilerplate'},
+	// 	{id:1, text:'AngularJS'},
+	// 	{id:2, text:'Karma'},
+	// 	{id:3, text:'Hello'},
+	// 	{id:4, text:'World'},
+	// 	{id:5, text:'How'},
+	// 	{id:6, text:'Are'},
+	// 	{id:7, text:'You'},
+	// 	{id:8, text:'?'},
+	// 	{id:9, text:'I'},
+	// 	{id:10, text:'write'},
+	// 	{id:11, text:'more'},
+	// 	{id:12, text:'to'},
+	// 	{id:13, text:'make'},
+	// 	{id:14, text:'the'},
+	// 	{id:15, text:'list'},
+	// 	{id:16, text:'longer'}
+	// ];
 
 	$scope.$watch('reverse', function () {
 		$scope.setOrder();
